5f3784c510d9885f5981a9581e034ff790ef2ea0
brandnewson <bransontay@gmail.com>
2025-09-08 06:27:11 +0100

    added track loading

--- src/main.py
+++ src/main.py
@@ -1,2 +1,33 @@
-# The start of the main script for the application
+import json
+import os
 
+def loadConfig(configPath):
+    with open(configPath, 'r') as f:
+        return json.load(f)
+
+configPath = os.path.abspath(os.path.join(os.path.dirname(__file__), '../config.json'))
+config = loadConfig(configPath)
+
+
+def main():
+
+    # TODO: Load vehicle data and track data, then run simulation
+
+    # Import modules
+    from track.track import loadTrack
+    # Create results dir
+
+    # Validation tests
+
+    # Load Track data
+    print("Using track filepath:", config["track"]["filepath"])
+    try: 
+        track = loadTrack(config["track"]["filepath"])
+        print(f"Loaded track with {len(track.points)} points, total length: {track.total_length:.2f} m")
+
+    except Exception as e:
+        print("Error loading track:", e)
+        return
+
+if __name__ == "__main__":
+    main()
